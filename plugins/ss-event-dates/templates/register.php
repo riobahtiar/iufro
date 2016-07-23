ce5fc27c858ba172a501b46ef56a0fa6295153b8
Rio Bahtiar <akhibahtiar@gmail.com>
2016-07-23 21:09:06 +0700

    change old functionto add users

--- plugins/ss-event-dates/templates/register.php
+++ plugins/ss-event-dates/templates/register.php
@@ -2,23 +2,6 @@
 $success = 0;
 
 if ( isset($_POST['submit'] ) ) {
-
-    registration_validation(
-        $_POST['fullname'],
-        $_POST['password'],
-        $_POST['email'],
-        $_POST['phone'],
-        $_POST['c_password'],
-        $_POST['address'],
-        $_POST['zip'],
-        $_POST['city'],
-        $_POST['state'],
-        $_POST['country'],
-        $_POST['user_type']
-    );
-       
-    // sanitize user form input
-    global $fullname, $password, $email, $phone, $address, $zip, $city, $state, $country, $user_reg_type;
     $fullname   =   sanitize_user( $_POST['salutation'].$_POST['title']." ".$_POST['fullname'] );
     $password   =   esc_attr( $_POST['password'] );
     $email      =   sanitize_email( $_POST['email'] );
@@ -28,80 +11,20 @@ if ( isset($_POST['submit'] ) ) {
     $city       =   sanitize_text_field( $_POST['city'] );
     $state      =   sanitize_text_field( $_POST['state'] );
     $country    =   sanitize_text_field( $_POST['country'] );
-    $user_reg_type   =  sanitize_text_field( $_POST['user_type'] );
- 
-       
-    
-    //save custom data into table  wp_ss_event_user_detail
-    save_custom_userdata(
-        $fullname, 
-        $password, 
-        $email, 
-        $phone, 
-        $address, 
-        $zip, 
-        $city, 
-        $state, 
-        $country,
-        $user_reg_type
-    );
+    $user_reg_type   =  $_POST['user_type'];
 
-    // call @function complete_registration to create the user
-    // only when no WP_error is found
-    // echo "here_2";
-    $success = complete_registration(
-        $fullname, 
-        $password, 
-        $email, 
-        $phone, 
-        $address, 
-        $zip, 
-        $city, 
-        $state, 
-        $country,
-        $user_reg_type
-    );
+    //save custom data into table  wp_ss_event_user_detail
+    save_custom_userdata($fullname, $password, $email, $phone, $address, $zip, $city, $state, $country,$user_reg_type);
+    $success = complete_registration($fullname, $password, $email, $phone, $address, $zip, $city, $state, $country,$user_reg_type);
+    echo "<br>Validate Data 1 ". $fullname." + ".$password." + ".$email." + ".$phone." + ".$address." + ".$zip." + ".$city." + ".$state." + ".$country." + ".$user_reg_type;
 }
 
 if ($success == 0){
     require_once dirname(__FILE__) . '/register/personal.php';
 }
 
-function registration_validation( $fullname, $password, $email, $phone, $c_password, $address, $zip, $city, $state, $country, $user_reg_type) {
-	global $reg_errors;
-	$reg_errors = new WP_Error;
-	if ( empty( $fullname ) || empty( $password ) || empty( $email ) || empty( $phone ) || empty( $c_password ) || empty( $zip ) || empty( $city ) || empty( $state ) || empty( $country ) || empty( $user_reg_type ) ) {
-    	$reg_errors->add('field', 'Required form field is missing');
-	}
-	if ( 5 > strlen( $password ) ) {
-        $reg_errors->add( 'password', 'Password length must be greater than 5' );
-    }
-    if ($password != $c_password ) {
-        $reg_errors->add( 'password', 'Password and confirmation password must be same' );
-    }
 
-    if ( !is_email( $email ) ) {
-	    $reg_errors->add( 'email_invalid', 'Email is not valid' );
-	}
-
-	if ( email_exists( $email ) ) {
-	    $reg_errors->add( 'email', 'Email Already in use' );
-	}
-
-	if ( is_wp_error( $reg_errors ) ) {
-	    foreach ( $reg_errors->get_error_messages() as $error ) {
-	        echo '<div>';
-	        echo '<strong>ERROR </strong>:';
-	        echo $error . '<br/>';
-	        echo '</div>';	         
-	    }	 
-	}
-}
-
-
-function complete_registration() {
-    global $reg_errors, $fullname, $password, $email, $phone, $c_password, $address, $zip, $city, $state, $country;
-    if ( 1 > count( $reg_errors->get_error_messages() ) ) {
+function complete_registration($fullname, $password, $email, $phone, $address, $zip, $city, $state, $country,$user_reg_type) {
         $userdata = array(
         'user_login'    =>   $email,
         'user_email'    =>   $email,
@@ -116,14 +39,11 @@ function complete_registration() {
         );
         $user = wp_insert_user( $userdata );
         echo 'Please login and complete your registeration here <a href="' . get_site_url() . '/login">login page</a>.'; 
-
-        return 1;
-    }
-    
+        echo "<br>Validate Data 2 ". $fullname." + ".$password." + ".$email." + ".$phone." + ".$address." + ".$zip." + ".$city." + ".$state." + ".$country." + ".$user_reg_type;
+        return 1;    
 }
 
-function save_custom_userdata(){
-    global $reg_errors, $fullname, $password, $email, $phone, $c_password, $address, $zip, $city, $state, $country, $user_reg_type;   
+function save_custom_userdata($fullname, $password, $email, $phone, $address, $zip, $city, $state, $country,$user_reg_type){
     global $wpdb;
     $wpdb->insert( 
                 'wp_ss_event_user_detail', 
@@ -150,7 +70,10 @@ function save_custom_userdata(){
                 '%s',
                 '%s') 
             );
+    echo "<br>Validate Data 3 ". $fullname." + ".$password." + ".$email." + ".$phone." + ".$address." + ".$zip." + ".$city." + ".$state." + ".$country." + ".$user_reg_type;
     }
+
+
 
 
 ?>
