d2bdbac477a45a97a5cced486f7e490b682537dc
Rio Bahtiar <akhibahtiar@gmail.com>
2016-07-25 17:07:05 +0700

    GUI Email

--- plugins/ss-event-dates/templates/register/pay_later.php
+++ plugins/ss-event-dates/templates/register/pay_later.php
@@ -2,10 +2,64 @@
 
 $to = 'riob@softwareseni.com';
 $subject = 'Yoww Laa Testing Pay Later';
-$body = 'From Rio Bahtiar IUFRO, Paylater Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi dolores dolore, earum doloribus, veritatis reiciendis qui numquam consequuntur nihil nostrum maiores, quas mollitia accusantium iure et. Laboriosam rem, aspernatur sit! <br> <strong>Is Bold text</strong><br><hr><br>send from: http://staging.iufroacacia2017.com';
+$body[] = '
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>IUFRO</title>
+        <style type="text/css">
+@media (max-width: 992px) {
+    #content {
+        max-width: 100% !important;
+        padding-left: 30px;
+        padding-right: 30px;
+    }
+    #logo img {
+        width: 100% !important;
+        max-width: 300px !important;
+    }
+}
+
+        </style>
+    </head>
+
+    <body style="font-family:Lato,Helvetica Neue,Helvetica,Arial,sans-serif;font-size:16px;color:#525252;margin:0;line-height:1.5;padding: 15px 0;background:#fff;">
+        <div id="content" style="max-width:550px;letter-spacing:0.5px; margin: 0 auto">
+            <div id="logo" style="background:#fff;overflow:hidden;padding:16px;text-align:center">
+                <img src="http://demosite.softwareseni.com/iufro/wp-content/uploads/2016/06/logo.png" alt="IufroLogo" style="min-height:30px;width:200px;max-width:100%;vertical-align:top" class="CToWUd">
+            </div>
+            <div>
+                <h2 style="text-align:center;color:#809062;margin-top: 0;">Dear Sir / Madam</h2>
+            </div>
+            <div style="background:#809062;color:#fff;font-size:14px;text-align:center;width:100%;padding: 15px 0;">
+';
+$body[] = 'Testing Graphical Email</div>';
+$body[] = '
+<div style="width:100%;text-align: left;border-bottom:1px solid #809062;">
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore soluta velit similique at suscipit porro debitis omnis cum? Quod repudiandae dignissimos illum consectetur nesciunt animi culpa aliquid provident incidunt nisi.  </p>
+            </div>
+';
+$body[] = '
+            <div>
+                <p style="font-size:16px;">We are looking forward to welcoming your guest at IUFRO ACACIA CONFERENCE 2017</p>
+                <p style="font-size:16px;">Should you require any further assistance, please do not hesitate to contact us on address below: </p>
+                <p style="font-size:12px;margin: 0;color:#809062">Center of Forest Biotechnology and Tree Improvement</p>
+                <p style="font-size:12px;margin: 0;color:#809062">Jl. Palagan Tentara Pelajar KM 15 Purwobinangun, Pakem, Sleman, Yogyakarta 55582</p>
+                <p style="font-size:12px;margin: 0;color:#809062">Phone: 0274-895954</p>
+                <p style="font-size:12px;margin: 0;color:#809062">Email: secretariat@iufroacacia2017.com</p>
+            </div>
+        </div>
+    </body>
+
+</html>
+';
+
+
+
 $headers[] = 'Content-Type: text/html; charset=UTF-8';
 $headers[] = 'From: IUFRO ACACIA TEAM <noreply@iufroacacia2017.com>';
-$headers[] = 'Cc: Gamma Aieska <gamma@softwareseni.com>';
 $headers[] = 'Cc: Rio Hotmail <riobahtiar@live.com>'; // note you can just use a simple email address
 wp_mail( $to, $subject, $body, $headers );
 echo "Check Your Email";
