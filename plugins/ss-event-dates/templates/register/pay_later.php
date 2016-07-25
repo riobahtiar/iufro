25e032517ed59a8203c4f6a52a37b55691d864d6
Rio Bahtiar <akhibahtiar@gmail.com>
2016-07-25 15:54:03 +0700

    fix test email lagi

---
+++ plugins/ss-event-dates/templates/register/pay_later.php
@@ -0,0 +1,13 @@
+<?php
+
+$to = 'riob@softwareseni.com';
+$subject = 'Yoww Laa Testing Pay Later';
+$body = 'From Rio Bahtiar IUFRO, Paylater Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi dolores dolore, earum doloribus, veritatis reiciendis qui numquam consequuntur nihil nostrum maiores, quas mollitia accusantium iure et. Laboriosam rem, aspernatur sit! <br> <strong>Is Bold text</strong>';
+$headers[] = 'Content-Type: text/html; charset=UTF-8';
+$headers[] = 'From: IUFRO ACACIA TEAM <noreply@iufroacacia2017.com>';
+$headers[] = 'Cc: Gamma Aieska <gamma@softwareseni.com>';
+$headers[] = 'Cc: Rio Hotmail <riobahtiar@live.com>'; // note you can just use a simple email address
+wp_mail( $to, $subject, $body, $headers );
+
+
+?>
