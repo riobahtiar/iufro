08a36923d8556e78f96ded86efddc9b456668bb7
Rio Bahtiar <akhibahtiar@gmail.com>
2016-08-01 13:39:29 +0700

    asdasd

--- plugins/ss-event-dates/templates/dashboard/edit_profile.php
+++ plugins/ss-event-dates/templates/dashboard/edit_profile.php
@@ -1,14 +1,14 @@
 <?php
 	$parse_uri = explode('wp-content', $_SERVER['SCRIPT_FILENAME']);
 	require_once( $parse_uri[0] . 'wp-load.php' );
-	
-	global $current_user;
-    wp_get_current_user();
-  	$euser_email = $current_user->user_email;
+	var_dump($parse_uri[0]);
+	// global $current_user;
+ //    wp_get_current_user();
+ //  	$euser_email = $current_user->user_email;
 
-	global $wpdb;
-	$query="SELECT * FROM wp_ss_event_user_detail WHERE euser_email = '{$euser_email}'";
-	$user_detail = $wpdb->get_row( $query, ARRAY_A );
+	// global $wpdb;
+	// $query="SELECT * FROM wp_ss_event_user_detail WHERE euser_email = '{$euser_email}'";
+	// $user_detail = $wpdb->get_row( $query, ARRAY_A );
 ?>
 <form id="profile-form">
 <div class="col-md-8">
