bc858ada3ebb89a3137a577313f230e835baacd6
Rio Bahtiar <akhibahtiar@gmail.com>
2016-08-02 17:38:06 +0700

    add test x2

--- plugins/ss-event-dates/addons/barcode/generate-verified-files.php
+++ plugins/ss-event-dates/addons/barcode/generate-verified-files.php
@@ -16,8 +16,8 @@ $generatorSVG = new Picqer\Barcode\BarcodeGeneratorSVG();
 file_put_contents('tests/verified-files/0049000004632-ean13.svg', $generatorSVG->getBarcode('0049000004632', $generatorSVG::TYPE_EAN_13));
 
 $generator = new \Picqer\Barcode\BarcodeGeneratorPNG();
-echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode('081231723897', $generator::TYPE_CODE_128)) . '"><br>';
-echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode('081231723897', $generator::TYPE_CODE_128_B)) . '"><br>';
-echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode('081231723897', $generator::TYPE_UPC_A)) . '"><br>';
-echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode('081231723897', $generator::TYPE_PLANET)) . '"><br>';
-echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode('081231723897', $generator::TYPE_PHARMA_CODE_TWO_TRACKS)) . '"><br>';
+echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode('081231723897', $generator::TYPE_CODE_128)) . '"><br><br><br><br>';
+echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode('04563897', $generator::TYPE_CODE_128_B)) . '"><br><br><br><br>';
+echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode('08145654697', $generator::TYPE_UPC_A)) . '"><br><br><br><br>';
+echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode('084565465465497', $generator::TYPE_PLANET)) . '"><br><br><br><br>';
+echo '<img src="data:image/png;base64,' . base64_encode($generator->getBarcode('08145654654697', $generator::TYPE_PHARMA_CODE_TWO_TRACKS)) . '"><br>';
