542b3cfcc0fceadb90229a65c2d96f7786884bfc
Rio Bahtiar <akhibahtiar@gmail.com>
2016-08-08 17:38:13 +0700

    payment IDR fix3.3

--- plugins/ss-event-dates/templates/register/payment.php
+++ plugins/ss-event-dates/templates/register/payment.php
@@ -227,12 +227,10 @@ if ($user_detail['euser_type'] == "local student") {
       <td colspan="2" > Net Total </td>
       <td>US$ <?php echo $total_price; ?> | IDR <?php
         $idr_total = $idr_good * $total_price; 
-
-setlocale(LC_MONETARY, 'id_ID');
-echo money_format('Rp. ', $idr_total) . "\n";
+        echo number_format($idr_total, 0, ".", ".");
 
 
-        echo $idr_total."*<br><br><br><hr>";
+        echo "*<br><br><br><hr>";
         echo "*- Current IDR Rates US$1 = IDR ".$idr_good."<br> Source : openexchangerates.org ";
        ?></td>
     </tr>
