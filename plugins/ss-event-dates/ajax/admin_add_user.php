a307b24fc8865691191345df90268c62493cb4b4
Rio Bahtiar <akhibahtiar@gmail.com>
2016-08-10 15:37:49 +0700

    fix payment page buggy v4

--- plugins/ss-event-dates/ajax/admin_add_user.php
+++ plugins/ss-event-dates/ajax/admin_add_user.php
@@ -14,7 +14,7 @@ $user_detail = $wpdb->
     <meta content="IE=edge" http-equiv="X-UA-Compatible">
     <meta content="width=device-width, initial-scale=1" name="viewport">
     <title></title>
-    <link crossorigin="anonymous" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" rel="stylesheet">
+    <link href="<?php echo plugins_url(); ?>/ss-event-dates/assets/bootstrap.min.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="http://lab.iamrohit.in/js/location.js"></script>
     </head>
