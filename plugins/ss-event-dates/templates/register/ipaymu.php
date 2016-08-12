f2493be612f78fe27787d1ad408569300712ed39
Rio Bahtiar <akhibahtiar@gmail.com>
2016-08-12 11:44:13 +0700

    finishing ipaymu

--- plugins/ss-event-dates/templates/register/ipaymu.php
+++ plugins/ss-event-dates/templates/register/ipaymu.php
@@ -11,16 +11,19 @@ $oxr_latest = json_decode($json);
 // URL Payment IPAYMU
 $url = 'https://my.ipaymu.com/payment.htm';
 
+
+
+
 // Prepare Parameters
 $params = array(
             'key'      => 'qy8HHvRg1j1HD1F5WG80wLSy0TTzY1', // API Key Merchant / Penjual
             'action'   => 'payment',
             'product'  => $_POST['item_name'],
-            'price'    => $_POST['amount'], // Total Harga
+            'price'    => 11997, // Total Harga
             'quantity' => 1,
             'comments' => 'Extra Details = Product Name: '.$_POST['item_name'].' - Barcode: '.$_POST['ebarcode'].' - Amount: '.$_POST['amount'].' - Time: '.date('Y-m-d H:i:s'), // Optional           
-            'ureturn'  =>   'http://staging.iufroacacia2017.com/login/user_dashboard?step=paypal_cancel',
-            'unotify'  =>   'http://staging.iufroacacia2017.com/wp-content/plugins/ss-event-dates/ajax/pyipn_v2.php?auth_code='.$_POST['ebarcode'],
+            'ureturn'  =>   'http://staging.iufroacacia2017.com//login/user_dashboard?step=paypal_success&trxname='.$_POST['item_name'],
+            'unotify'  =>   'http://staging.iufroacacia2017.com/wp-content/plugins/ss-event-dates/ajax/ipaymu_notify.php?auth_code='.$_POST['ebarcode'],
             'ucancel'  =>   'http://staging.iufroacacia2017.com/login/user_dashboard?step=paypal_cancel',
             'format'   => 'json' // Format: xml / json. Default: xml 
         );
