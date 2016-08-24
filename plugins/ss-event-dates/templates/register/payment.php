ad1396806251fbf5c3f370dd7b1d58012101f577
Rio Bahtiar <akhibahtiar@gmail.com>
2016-08-24 13:54:10 +0700

    dates vj

--- plugins/ss-event-dates/templates/register/payment.php
+++ plugins/ss-event-dates/templates/register/payment.php
@@ -300,7 +300,7 @@ echo "Current IDR Rates US$1 = IDR " . $idr_good . "<br> Source : <a href='http:
 $today  = strtotime(date('Y-m-d'));
 $closed = strtotime($ss_theme_opt['date_close']);
 // Check if Payment Done or Onsite
- if ( $user_detail['euser_payment_status'] !== 'onsite-payment' || $user_detail['euser_payment_status'] !== 'berhasil-iPaymu' || $user_detail['euser_payment_status'] !== 'Completed-Paypal' || $today > $closed ) { ?>
+ if ( $user_detail['euser_payment_status'] !== 'onsite-payment' || $user_detail['euser_payment_status'] !== 'berhasil-iPaymu' || $user_detail['euser_payment_status'] !== 'Completed-Paypal' ||  $closed > $today ) { ?>
 
     <button type="submit" name="submit" class="btn btn-default pull-right" value="payment">Pay Now</button>
     <a href="<?php echo get_permalink(); ?>?step=pay_later" class="btn btn-default pull-right">Pay Later</a>
