e129e4dd66a7fa6cb7a82952d41aada20cd41073
Rio Bahtiar <akhibahtiar@gmail.com>
2016-08-29 13:46:57 +0700

    testing payment status 6.fixed

--- plugins/ss-event-dates/templates/register/payment.php
+++ plugins/ss-event-dates/templates/register/payment.php
@@ -22,16 +22,6 @@ global $wpdb;
 global $ss_theme_opt; 
 $query       = "SELECT * FROM wp_ss_event_user_detail WHERE euser_email = '{$euser_email}'";
 $user_detail = $wpdb->get_row($query, ARRAY_A);
-?>
-<pre>
-    <?php echo $user_detail['euser_payment_status'].'
-     <-> '.$closed.$today;
-echo "<br>";
-var_dump($user_detail['euser_payment_status']);
-
-      ?>
-</pre>
-<?php
 // ======== Start Payment Conditional Block ======== //
 
 // mid conf
@@ -328,14 +318,7 @@ echo "<br><p><small>*Current IDR Rates US$1 = IDR " . $idr_good . "<br> Source :
 $today  = strtotime(date('Y-m-d'));
 $closed = strtotime($ss_theme_opt['date_close']);
 // Check if Payment Done or Onsite
- if ( $user_detail['euser_payment_status'] !== 'onsite-payment' || $user_detail['euser_payment_status'] !== 'berhasil-iPaymu' || $user_detail['euser_payment_status'] !== 'Completed-Paypal' ||  $closed > $today ) { ?>
-<pre>
-    <?php echo $user_detail['euser_payment_status'].'
-     <-> '.$closed.$today;
-echo "<br>";
-var_dump($user_detail['euser_payment_status']);
-      ?>
-</pre>
+ if ( $user_detail['euser_payment_status'] !== "onsite-payment" || $user_detail['euser_payment_status'] !== "berhasil-iPaymu" || $user_detail['euser_payment_status'] !== "Completed-Paypal" ||  $closed > $today ) { ?>
     <button type="submit" name="submit" class="btn btn-default pull-right" value="payment">Pay Now</button>
     <a href="<?php echo get_permalink(); ?>?step=pay_later" class="btn btn-default pull-right">Pay Later</a>
 <?php } } ?>
